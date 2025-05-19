ebfbb72924a029f4e96381a828ba9a368ab2d357
Garrison <mullenga@tcd.ie>
2025-05-18 22:26:22 -0400

    global variable support

--- com/karp/Interpreter.java
+++ com/karp/Interpreter.java
@@ -3,6 +3,8 @@ package com.karp;
 import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+    private Environment environment = new Environment();
+
     void interpret(List<Stmt> statements) {
         try {
             for (Stmt statement : statements) {
@@ -31,6 +33,17 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     public Void visitSayStmt(Stmt.Say stmt) {
         Object value = evaluate(stmt.expression);
         System.out.println(stringify(value));
+        return null;
+    }
+
+    @Override
+    public Void visitVarStmt(Stmt.Var stmt) {
+        Object value = null;
+        if (stmt.initializer != null) {
+            value = evaluate(stmt.initializer);
+        }
+
+        environment.define(stmt.name.lexeme, value);
         return null;
     }
 
@@ -101,6 +114,11 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
             default: return null; // unreachable
         }
+    }
+
+    @Override
+    public Object visitVariableExpr(Expr.Variable expr) {
+        return environment.get(expr.name);
     }
 
     private void checkNumberOperand(Token operator, Object operand) {
