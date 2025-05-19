3dcde735739f05497bc179585f8860cc91d6ee66
garrison-pc <mullenzen@gmail.com>
2025-05-18 23:47:32 -0400

    variable creation, access, and modification now done (global only, im gonna do scope tmr)

--- com/tool/GenerateAst.java
+++ com/tool/GenerateAst.java
@@ -2,8 +2,8 @@ package com.tool;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
 import java.util.Arrays;
+import java.util.List;
 
 public class GenerateAst {
     public static void main(String[] args) throws IOException {
@@ -13,7 +13,8 @@ public class GenerateAst {
         }
         String outputDir = args[0];
         defineAst(outputDir, "Expr", Arrays.asList(
-       "Binary   : Expr left, Token operator, Expr right",
+            "Assign   : Token name, Expr value",
+            "Binary   : Expr left, Token operator, Expr right",
             "Grouping : Expr expression",
             "Literal  : Object value",
             "Unary    : Token operator, Expr right",
