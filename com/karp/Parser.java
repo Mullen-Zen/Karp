588cdeffaf7199887a8c8a524568d8b97efaef2b
garrison-pc <mullenzen@gmail.com>
2025-07-01 09:46:10 -0400

    WERE SO BACK started work on adding oop stuff to karplang

--- com/karp/Parser.java
+++ com/karp/Parser.java
@@ -28,6 +28,7 @@ class Parser {
     // each grammar rule becomes a method
     private Stmt declaration() {
         try {
+            if (match(CLASS)) return classDeclaration();
             if (match(FUNC)) return function("function");
             if (match(VAR)) return varDeclaration();
 
@@ -36,6 +37,20 @@ class Parser {
             synchronize();
             return null;
         }
+    }
+
+    private Stmt classDeclaration() {
+        Token name = consume(IDENTIFIER, "Expect class name.");
+        consume(LEFT_BRACE, "Expect '{' before class body.");
+
+        List<Stmt.Function> methods = new ArrayList<>();
+        while(!check(RIGHT_BRACE) && !isAtEnd()) {
+            methods.add(function("method"));
+        }
+
+        consume(RIGHT_BRACE, "Expect '}' after class body.");
+
+        return new Stmt.Class(name, methods);
     }
 
     private Stmt varDeclaration() {
